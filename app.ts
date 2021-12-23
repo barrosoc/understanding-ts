2543aaf347fb35648f0d21575ff5a9656216815a
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2021-12-23 11:42:02 +0100

    Objects & Arrays & Tuples

--- app.ts
+++ app.ts
@@ -1,15 +1,17 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
-    if(showResult === true) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
+    name:  'Carlos',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+
+console.log(person.name);
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());   
 }
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
