8d8c850d414c98841a7b8db864de4120ce33b1af
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2021-12-23 18:17:43 +0100

    Static Methods & Properties

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 class Department {
+  static ficalYear = 2021;
   protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {}
+
+  static createEmployee(name: string) {
+    return { name };
+  }
 
   describe(this: Department) {
     console.log(`Department (${this.id}): ` + this.name);
   }
 
   addEmployee(employee: string) {
-    this.employees.push(employee)
+    this.employees.push(employee);
   }
 
   printEmployeeInformation() {
@@ -20,7 +25,7 @@ class Department {
 class ITDepartment extends Department {
   admins: string[];
   constructor(id: string, admins: string[]) {
-    super(id, 'IT');
+    super(id, "IT");
     this.admins = admins;
   }
 }
@@ -29,27 +34,27 @@ class AccountingDepartment extends Department {
   private lastReport: string;
 
   get mostRecentReport() {
-    if(this.lastReport) {
+    if (this.lastReport) {
       return this.lastReport;
     }
-    throw new Error('No report found.');
+    throw new Error("No report found.");
   }
 
   set mostRecentReport(value: string) {
-    if(!value) {
-      throw new Error('Value cannot be undefined.');
+    if (!value) {
+      throw new Error("Value cannot be undefined.");
     }
     this.addReport(value);
   }
 
   constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting');
+    super(id, "Accounting");
     this.lastReport = reports[0];
   }
 
   addEmployee(employee: string) {
-    if (employee === 'Carlos'){
-      return
+    if (employee === "Carlos") {
+      return;
     }
     super.addEmployee(employee);
   }
@@ -59,18 +64,20 @@ class AccountingDepartment extends Department {
     this.lastReport = text;
   }
 
-  printReports(){
+  printReports() {
     console.log(this.reports);
   }
 }
 
-const accounting = new AccountingDepartment('d1', []);
-accounting.mostRecentReport = 'Year End Report.'
-accounting.addReport('Error...');
+const employee1 = Department.createEmployee('Manuel');
+console.log(employee1, Department.ficalYear);
+
+const accounting = new AccountingDepartment("d1", []);
+accounting.mostRecentReport = "Year End Report.";
+accounting.addReport("Error...");
 accounting.mostRecentReport;
 accounting.describe();
-accounting.addEmployee('Carlos');
-accounting.addEmployee('Ana');
-
+accounting.addEmployee("Carlos");
+accounting.addEmployee("Ana");
 
 accounting.printReports();
