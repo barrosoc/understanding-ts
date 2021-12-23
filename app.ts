9d01f95e7b900d90de987057ab81f9ceb0e04925
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2021-12-23 12:47:26 +0100

    Literals

--- app.ts
+++ app.ts
@@ -1,15 +1,28 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string, 
+  input2: number | string, 
+  resultCoversion: 'as-number' | 'as-text'
+  ) {
   let result;
-  if(typeof input1 === 'number' && typeof input2 === 'number') {
-      result = input1 + input2;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  return result;
+  if(resultCoversion === 'as-number') {
+    return +result;
+  } else {
+    return result.toString();
+  }
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Carlos', 'Ana');
+
+
+const combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine("Carlos", "Ana", 'as-text');
 console.log(combineNames);
