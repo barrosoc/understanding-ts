50bf794ad7f1605169a46615d57fc5145426481e
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2021-12-23 18:05:38 +0100

    Overriding Properties & The "protected" Modifier

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {}
 
@@ -30,6 +30,13 @@ class AccountingDepartment extends Department {
     super(id, 'Accounting');
   }
 
+  addEmployee(employee: string) {
+    if (employee === 'Carlos'){
+      return
+    }
+    super.addEmployee(employee);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -42,5 +49,8 @@ class AccountingDepartment extends Department {
 const accounting = new AccountingDepartment('d1', []);
 accounting.describe();
 accounting.addReport('Error...');
+accounting.addEmployee('Carlos');
+accounting.addEmployee('Ana');
+
 
 accounting.printReports();
