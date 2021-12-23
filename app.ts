e7f70e7984f63bfe6f87920dd188e6785851013b
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2021-12-23 15:38:28 +0100

    Clear TS compilation errors

---
+++ app.ts
@@ -0,0 +1,3 @@
+const forename = 'Carlos';
+
+console.log(forename);
--- types/basics.ts
+++ types/basics.ts
@@ -1,4 +1,4 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function sum(n1: number, n2: number, showResult: boolean, phrase: string) {
     const result = n1 + n2;
     if(showResult === true) {
         console.log(phrase + result);
@@ -9,7 +9,7 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 
 const number1 = 5;
 const number2 = 2.8;
-const printResult = true;
+const showResult = true;
 const resultPhrase = 'Result is: ';
 
-add(number1, number2, printResult, resultPhrase);
+sum(number1, number2, showResult, resultPhrase);
