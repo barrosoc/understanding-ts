3db2760c1630ba2c241f1a8e4a2db3decb4bb3a5
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2021-12-23 17:40:14 +0100

    Constructor Functions & The "this" Keyword

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
-const button = document.querySelector('button')!;
+class Department {
+  name: string;
 
+  constructor(name: string) {
+    this.name = name;
+  }
 
-button.addEventListener('click', () => {
-  console.log('Clicked!');
-})
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department('Carlos');
+accounting.describe();
+
+const accountingCopy = { name: 'Ana', describe: accounting.describe };
+accountingCopy.describe();
