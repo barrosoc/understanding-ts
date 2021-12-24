b44026d104e71e5c3cbb8f786e6d7811f9b3318f
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2021-12-24 11:13:05 +0100

    First Interface

---
+++ src/classess.ts
@@ -0,0 +1,100 @@
+abstract class Department {
+  static ficalYear = 2021;
+  protected employees: string[] = [];
+
+  constructor(protected readonly id: string, public name: string) {}
+
+  static createEmployee(name: string) {
+    return { name };
+  }
+
+  abstract describe(this: Department): void;
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+
+
+
+  describe() {
+    console.log('IT department id: ' + this.id);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Value cannot be undefined.");
+    }
+    this.addReport(value);
+  }
+
+  private constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if(AccountingDepartment.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment('d2', []);
+    return this.instance;
+  }
+
+  describe() {
+    console.log('Accounting department id: ' + this.id);
+  }
+
+  addEmployee(employee: string) {
+    if (employee === "Carlos") {
+      return;
+    }
+    super.addEmployee(employee);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const employee1 = Department.createEmployee('Manuel');
+console.log(employee1, Department.ficalYear);
+
+const accounting = AccountingDepartment.getInstance();
+accounting.mostRecentReport = "Year End Report.";
+accounting.addReport("Error...");
+accounting.mostRecentReport;
+accounting.describe();
+accounting.addEmployee("Carlos");
+accounting.addEmployee("Ana");
+
+accounting.printReports();
