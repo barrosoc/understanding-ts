a817b432420b2bea0823269ad834b337b2f35637
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-03 16:46:20 +0100

    Discriminated Unions

--- src/app.ts
+++ src/app.ts
@@ -73,3 +73,31 @@ const e1: ElevatedEmployee = {
 
  useVehicle(v1);
  useVehicle(v2);
+
+
+interface Bird {
+  type: 'bird'
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse'
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch(animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+  console.log('Moving with speed...' + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 100});
+moveAnimal({type: 'horse', runningSpeed: 50});
