38d9f021dc09bb6f83c4a08ab45ba38d0fe10a10
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-03 16:36:45 +0100

    More on Type Guards

--- src/app.ts
+++ src/app.ts
@@ -21,3 +21,55 @@ const e1: ElevatedEmployee = {
  type Numeric = number | boolean;
 
  type Universal = Combine & Numeric;
+
+ function sumAdd(a: Combine, b: Combine) {
+   if(typeof a === 'string' || typeof b === 'string') {
+     return a.toString() + b.toString();
+   }
+   return a + b;
+ }
+
+ type UnkownEmployee = Employee | Admin;
+
+ function printEmployeeInformation(emp: UnkownEmployee) {
+   console.log('Name: ' + emp.name);
+   if('priviledges' in emp) {
+     console.log('Priviledges: ' + emp.priviledges);
+   }
+   if('startDate' in emp) {
+     console.log('Start Date: ' + emp.startDate);
+   }
+ }
+
+ printEmployeeInformation({name: 'Carlos', startDate: new Date()});
+
+ class Car {
+   drive() {
+     console.log('Driving...');
+   }
+ }
+
+ class Truck {
+   drive() {
+     console.log('Driving a truck...');
+   }
+
+   loadCargo(amount: number){
+    console.log('Loading a cargo...' + amount);
+   }
+ }
+
+ type Vehicle = Car | Truck;
+
+ const v1 = new Car();
+ const v2 = new Truck();
+
+ function useVehicle(vehicle: Vehicle) {
+   vehicle.drive();
+   if(vehicle instanceof Truck) {
+     vehicle.loadCargo(1000);
+   }
+ }
+
+ useVehicle(v1);
+ useVehicle(v2);
