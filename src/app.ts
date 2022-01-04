c9f31b0057f85dd125cf89cf3138a7f21adeeff1
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-04 12:27:56 +0100

    Generic Classes

--- src/app.ts
+++ src/app.ts
@@ -40,3 +40,35 @@ function extractAndCovert<T extends object, U extends keyof T>(obj: T, key: U) {
 }
 
 extractAndCovert({name: 'Carlos'}, 'name');
+
+class DataStorage<T extends number | string | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T){
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if(this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Carlos');
+textStorage.addItem('Manuel');
+textStorage.removeItem('Carlos');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(10);
+numberStorage.removeItem(1);
+console.log(numberStorage.getItems());
