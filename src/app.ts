cc92c15c206b1054a9dbd2bce31c21807a793aba
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-04 13:36:06 +0100

    Adding Multiple Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 function Logger(logString: string) {
+  console.log('LOGGER factory');
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
   }
 }
 
-function WithTemplate(template: string, hookId: string){
+function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE factory');
   return function(constructor: any){
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
@@ -16,6 +18,7 @@ function WithTemplate(template: string, hookId: string){
   }
 }
 
+@Logger('LOGGING')
 @WithTemplate('<h1>My Person object</h1>', 'app')
 class Persona {
   name = 'Carlos';
