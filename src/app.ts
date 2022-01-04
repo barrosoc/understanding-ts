b05621acc08cdc8c6042167143b518c2ada4b8de
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-04 13:47:18 +0100

    Diving into Property Decorators

--- src/app.ts
+++ src/app.ts
@@ -31,3 +31,30 @@ class Persona {
 const pers = new Persona();
 
 console.log(pers);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator');
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+
+  set price(val: number) {
+    if(val < 0) {
+      throw new Error('Invalid price - should be positive') 
+    } 
+    this.price = val;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
