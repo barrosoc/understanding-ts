034b980fe701bf1429bfead2afe65b7fac9b0c9b
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-04 12:04:23 +0100

    Another Generic Function

--- src/app.ts
+++ src/app.ts
@@ -17,3 +17,20 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 const mergedObj = merge({name: 'Carlos', hobbies: ['Football']}, {age: 33});
 
 console.log(mergedObj.hobbies[0]);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndPrint<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value';
+
+  if(element.length === 1) {
+    descriptionText = 'Got one element'
+  } else {
+    descriptionText = `Got ${element.length} elements`;
+  }
+  return [element, descriptionText]
+}
+
+console.log(countAndPrint(['sports', 'cooking']));
