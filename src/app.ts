cddd4a3f349846dc3fd3b09f32006d2093115183
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-04 10:54:34 +0100

    Index Properties

--- src/app.ts
+++ src/app.ts
@@ -109,3 +109,12 @@ const userInputElement = document.getElementById('user-input');
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = 'Hi there!';
 }
+
+interface ErrorContainer {
+  [prop: string]: string
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!',
+  username: 'Must start with capital letter!'
+}
