a87414c4627faeedaed3c14eeb74488e97af5c8f
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-05 11:56:10 +0100

    When Do Decorators Execute?

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
 function Logger(logString: string) {
-  console.log('LOGGER factory');
+  console.log("LOGGER factory");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
-  }
+  };
 }
 
 function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE factory');
-  return function(constructor: any){
+  console.log("TEMPLATE factory");
+  return function (constructor: any) {
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
-    if(hookEl) {
+    if (hookEl) {
       hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
-  }
+  };
 }
 
-@Logger('LOGGING')
-@WithTemplate('<h1>My Person object</h1>', 'app')
+@Logger("LOGGING")
+@WithTemplate("<h1>My Person object</h1>", "app")
 class Persona {
-  name = 'Carlos';
+  name = "Carlos";
 
-  constructor(){
-    console.log('Creating person object');
+  constructor() {
+    console.log("Creating person object");
   }
 }
 
@@ -33,26 +33,34 @@ const pers = new Persona();
 console.log(pers);
 
 function Log(target: any, propertyName: string | Symbol) {
-  console.log('Property decorator!');
+  console.log("Property decorator!");
   console.log(target, propertyName);
 }
 
-function Log2(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-  console.log('Accessor decorator');
+function Log2(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Accessor decorator");
   console.log(target);
   console.log(name);
   console.log(descriptor);
 }
 
-function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-  console.log('Method decorator!');
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
   console.log(target);
   console.log(name);
   console.log(descriptor);
 }
 
 function Log4(target: any, name: string | Symbol, position: number) {
-  console.log('Parameter decorator!');
+  console.log("Parameter decorator!");
   console.log(target);
   console.log(name);
   console.log(position);
@@ -70,9 +78,9 @@ class Product {
 
   @Log2
   set price(val: number) {
-    if(val < 0) {
-      throw new Error('Invalid price - should be positive') 
-    } 
+    if (val < 0) {
+      throw new Error("Invalid price - should be positive");
+    }
     this.price = val;
   }
 
@@ -81,3 +89,6 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+const p1 = new Product('Book', 20);
+const p2 = new Product('Book2', 30);
