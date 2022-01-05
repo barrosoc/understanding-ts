0982aea7943af5e5e96fc74746144acd42505686
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-01-05 11:51:00 +0100

    Accessor & Parameter Decorators

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,29 @@ const pers = new Persona();
 console.log(pers);
 
 function Log(target: any, propertyName: string | Symbol) {
-  console.log('Property decorator');
+  console.log('Property decorator!');
   console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('Method decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(position);
 }
 
 class Product {
@@ -47,6 +68,7 @@ class Product {
     this._price = price;
   }
 
+  @Log2
   set price(val: number) {
     if(val < 0) {
       throw new Error('Invalid price - should be positive') 
@@ -54,7 +76,8 @@ class Product {
     this.price = val;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax);
   }
 }
